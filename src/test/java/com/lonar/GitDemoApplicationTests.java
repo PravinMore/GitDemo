587dd3e32182a1316d8f16b79a0c9efce2f41955
PRAVIN <pravinmore708@gmail.com>
2020-07-11 22:46:10 -0700

    Initial Commit

---
+++ src/test/java/com/lonar/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
