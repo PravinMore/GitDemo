3b4a7c5313d435f0cb89422718793e24832bc79f
PRAVIN <pravinmore708@gmail.com>
2020-07-22 09:37:21 -0700

    added first commit

--- src/main/java/com/lonar/GitDemoApplication.java
+++ src/main/java/com/lonar/GitDemoApplication.java
@@ -10,6 +10,7 @@ public class GitDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoApplication.class, args);
 		System.out.println("this is not corrects");
+		System.out.println("this is first");
 	}
 
 }
