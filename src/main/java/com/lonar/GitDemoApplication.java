588608d4638972a44545b3705e077410bae24a2f
PRAVIN <pravinmore708@gmail.com>
2020-07-22 09:34:19 -0700

    added new line

--- src/main/java/com/lonar/GitDemoApplication.java
+++ src/main/java/com/lonar/GitDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("This is the first statement");
+		
 		SpringApplication.run(GitDemoApplication.class, args);
 		System.out.println("this is the second line1111_1");
 	}
